ffd5b2989ac078874368feadc499230637211fc7
danielforeroj <daniforeroj@gmail.com>
2026-01-23 16:29:12 -0500

    Clean up TODO comments in sponsors data
    Removed TODO comments for sponsor entries.

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -5,25 +5,21 @@ export type Sponsor = {
 };
 
 export const sponsors: Sponsor[] = [
-  // TODO: Replace with real sponsor entries + real logo filenames.
   {
     name: "Tectonic",
     logoSrc: "/sponsors/tectonic-logo.png",
     href: "https://tectonic.xyz",
   },
-  // TODO: Replace with a real sponsor entry.
   {
     name: "Hack",
     logoSrc: "/sponsors/hack-logo.png",
   },
-  // TODO: Replace with a real sponsor entry.
   {
     name: "W3JOE",
     logoSrc: "/sponsors/w3joe-logo.png",
   },
-    {
+  {
     name: "Hexaco",
     logoSrc: "/sponsors/hexaco-logo.png",
   },
-
 ];
